703820df11e99e70684e2619bbd118bffce637db
navi123kumar <naveen.kumar_mca18@gla.ac.in>
2019-02-21 11:20:14 +0530

    change name

--- app/src/main/java/gla/gla1/MainActivity.java
+++ app/src/main/java/gla/gla1/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Naveen prajapati",
+                Snackbar.make(view, "Navin prajapati",
                         Snackbar.LENGTH_LONG)
                         .setAction("ok", null).show();
             }
