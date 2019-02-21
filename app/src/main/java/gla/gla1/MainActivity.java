addaaaacf9a2e894cce0c31785f37dcc86e5dbe0
navi123kumar <naveen.kumar_mca18@gla.ac.in>
2019-02-21 11:19:28 +0530

    change surname

--- app/src/main/java/gla/gla1/MainActivity.java
+++ app/src/main/java/gla/gla1/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Naveen kumar",
+                Snackbar.make(view, "Naveen prajapati",
                         Snackbar.LENGTH_LONG)
                         .setAction("ok", null).show();
             }
