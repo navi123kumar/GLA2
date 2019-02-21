d2e52a8073488109feae32681a16a44ed6c1244f
navi123kumar <naveen.kumar_mca18@gla.ac.in>
2019-02-21 11:23:07 +0530

    add two snackbar

--- app/src/main/java/gla/gla1/MainActivity.java
+++ app/src/main/java/gla/gla1/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Navin prajapati",
+                Snackbar.make(view, "Navin prajapati,mca student",
                         Snackbar.LENGTH_LONG)
                         .setAction("ok", null).show();
             }
